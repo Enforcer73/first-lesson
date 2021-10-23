a6e4b010e6ad1bd342871d874510215f0d0107b6
Ruslan Bagautdinov <bagarus73@gmail.com>
2021-10-23 07:42:50 +0400

    add comments

--- first lesson/ViewController.swift
+++ first lesson/ViewController.swift
@@ -32,10 +32,12 @@ final class ViewController: UIViewController {
         
     }
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
     
     @IBAction func pressedButton(_ sender: UIButton) {
         switch sender.currentTitle {
